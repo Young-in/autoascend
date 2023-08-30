28bb309df1a3bcef96095b3d75b6d65be8f5d71a
Young-in <daddy321@snu.ac.kr>
2023-08-30 18:56:32 +0900

    Record random seed

--- track_aa.py
+++ track_aa.py
@@ -4,18 +4,21 @@ from autoascend.env_wrapper import EnvWrapper
 import h5py
 import numpy as np
 from tqdm import trange
+from concurrent.futures import ThreadPoolExecutor
 
 import traceback
 
 env = EnvWrapper(gym.make('NetHackChallenge-v0', character='mon-hum-neu', no_progress_timeout=1000))
 
-with h5py.File('/storage/youngin/nld_aaa_test.hdf5', 'w') as f:
-    n = 5
+with h5py.File('/storage/youngin/nld_aaa.hdf5', 'w') as f:
+    n = 1000
     n_failed = 0
     for i in trange(n):
         try:
             env.seed(i, i)
-            env.main()
+            with ThreadPoolExecutor() as executor:
+                future = executor.submit(env.main)
+                future.result(timeout = 720)
             run_info = "success"
         except Exception as e:
             n_failed += 1
@@ -36,5 +39,6 @@ with h5py.File('/storage/youngin/nld_aaa_test.hdf5', 'w') as f:
             grp.create_dataset("score", data = summ['score'])
             grp.create_dataset("turns", data = summ['turns'])
             grp.create_dataset("run_info", data = run_info)
+            grp.create_dataset("seed", data = summ['seed'])
 
     print(f"fail ratio:{n_failed / n}")
